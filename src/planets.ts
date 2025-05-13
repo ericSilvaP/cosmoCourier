61c225fdbc202416610259d709b9c424859a6195
Eric Silva <ericsilvap13@gmail.com>
2025-05-13 09:45:10 -0300

    planets

---
+++ src/planets.ts
@@ -0,0 +1,5 @@
+export class Planet {
+  constructor(private distanceFromEarth: number, private atmosphereType: string, private cargo: string, private landingViability: number, private composition: string){}
+
+
+} 
