0b9d5685bed0bc9eb4c96bf83659ef6e2e9e7156
Eric Silva <ericsilvap13@gmail.com>
2025-05-13 16:16:37 -0300

    comeco main

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,21 @@
+import { Cargo } from "./cargos";
+import { Planet } from "./planets";
+import { CargoType } from "./types/cargoType";
+import { AtmosphereType } from "./types/planetAtmosphere";
+import { PlanetComposition } from "./types/planetComposition";
 
-const compositions = ["rocky", "gaseous", "ice giant", "ice dwarf", "volcanic", "exotic"]
+const compositions: string[] = [PlanetComposition.EXOTIC, PlanetComposition.GASEOUS, PlanetComposition.ICE_DWARF, PlanetComposition.ICE_GIANT, PlanetComposition.ROCKY, PlanetComposition.VOLCANIC]
+
+const atmospheres: string[] = [AtmosphereType.AMMONIA_RICH, AtmosphereType.CARBON_DIOXIDE_RICH, AtmosphereType.HYDROGEN_RICH, AtmosphereType.METHANE_RICH, AtmosphereType.NITROGEN_RICH, AtmosphereType.NONE, AtmosphereType.OXYGEN_RICH, AtmosphereType.SULFURIC]
+
+const cargoTypes: string[] = [CargoType.BIOLOGICAL, CargoType.COLONIZATION, CargoType.INDUSTRIAL, CargoType.MEDICAL, CargoType.SCIENTIFIC]
+
+function random(maxN: number): number {
+  return Math.floor(Math.random()*maxN)
+}
+
+const planets: Planet[] = []
+
+const nPlanets = random(10)
+const nCargos = random(10)
+const nSpaceships = random(10)
